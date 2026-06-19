341ae9f411be359f09f0f1c2bc5f44546130fac3
Hunsain Mazhar <22101001-008@uskt.edu.pk>
2026-06-19 23:01:34 +0500

    first-commit-9

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -204,6 +204,15 @@ function RootShell({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-960HCELD6N"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-960HCELD6N');
+        </script>
         <HeadContent />
 
         <script
